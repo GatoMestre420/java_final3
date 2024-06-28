00b8099fa841fcd516bc942c61441b8986d56df6
Gabriel Lincon <glincon44@gmail.com>
2024-06-28 00:15:31 -0300

    Organizador mvc

---
+++ src/br/edu/up/model/Participante.java
@@ -0,0 +1,19 @@
+package br.edu.up.model;
+
+public class Participante {
+    private String nome;
+
+    public Participante(String nome) {
+        this.nome = nome;
+    }
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
